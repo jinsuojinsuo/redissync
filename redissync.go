60263c832cf0bd2749d962c4dcb43cb849d039f7
liujinsuo <1027976838@qq.com>
2025-02-12 14:27:19 +0800

    redis锁与本地锁结合来提高性能

--- redissync.go
+++ redissync.go
@@ -6,67 +6,41 @@ import (
 	"github.com/bsm/redislock"
 	"github.com/go-redis/redis/v8"
 	"math/rand"
-	"path"
-	"runtime"
 	"runtime/debug"
-	"strings"
+	"sync"
+	"sync/atomic"
 	"time"
-)
-
-var (
-	// ErrNotObtained 当无法获得锁时，将返回ErrNotObtained
-	ErrNotObtained = redislock.ErrNotObtained
-
-	// ErrLockNotHeld 尝试释放非活动锁时返回ErrLockNotHeld
-	ErrLockNotHeld = redislock.ErrLockNotHeld
 )
 
 type Logger interface {
 	Println(v ...any)
 }
 
-type Lock struct {
-	rdl      *redislock.Lock
-	ctx      context.Context
-	cancel   context.CancelFunc //取消函数
-	key      string             //加锁的key
-	ttl      time.Duration      //redis中的键保存时长
-	logger   Logger             //用于记录错误日志
-	metadata string
-}
-
-// Unlock 解锁
-func (l *Lock) Unlock() error {
-	defer l.cancel()
-	if err := l.rdl.Release(l.ctx); err == redislock.ErrLockNotHeld {
-		return nil //未加锁不需要解锁
-	} else if err != nil {
-		return err
-	}
-	return nil
-}
-
-// Token 获取锁中存的值
-func (l *Lock) Token() string {
-	return l.rdl.Token()
-}
-
 type RedisSync struct {
 	redisLockClient *redislock.Client
-	logMode         bool //true开启日志 false关闭日志
+	m               map[string]*t1 //每个锁一个key 问题:所有线程都解锁后无法释放map的key
+	lock            sync.Mutex     //互斥锁 用户锁m,m虽然是支持并发的map但又可能需要同时操作两次
+	logMode         bool           //true开启日志 false关闭日志
 	logger          Logger
 }
 
 func NewRedisSync(rdb *redis.Client) *RedisSync {
 	return &RedisSync{
 		redisLockClient: redislock.New(rdb),
+		m:               map[string]*t1{},
 	}
 }
 
-//func (s *RedisSync) SetMetadata(metadata string) *RedisSync {
-//	s.metadata = metadata
-//	return s
-//}
+func (s *RedisSync) GetM() map[string]*t1 {
+	return s.m
+}
+
+// 日志打印
+func (s *RedisSync) println(v ...any) {
+	if s.logger != nil {
+		s.logger.Println(v...)
+	}
+}
 
 // SetLogger 设置日志
 func (s *RedisSync) SetLogger(logger Logger) *RedisSync {
@@ -74,66 +48,50 @@ func (s *RedisSync) SetLogger(logger Logger) *RedisSync {
 	return s
 }
 
-// TryLock 尝试加锁，非阻塞，加锁失败直接返回
-// ttl 设置加锁时长,设置redis键的过期时间
-// 可以自动续期
-func (s *RedisSync) TryLock(key string, ttl time.Duration) (*Lock, error) {
-	//ttl不能小于1秒
-	if ttl < time.Second {
-		ttl = time.Second
-	}
-
-	metadata := getParentCaller()
-
-	ctx, cancel := context.WithCancel(context.Background())
-	rdlLock, err := s.redisLockClient.Obtain(ctx, key, ttl, &redislock.Options{
-		RetryStrategy: redislock.NoRetry(), //不重试
-		Metadata:      metadata,
-	})
-	if err != nil {
-		cancel()
-		return nil, err
-	}
-	l := &Lock{
-		rdl:      rdlLock,
-		ctx:      ctx,
-		key:      key,
-		cancel:   cancel,
-		ttl:      ttl,
-		metadata: metadata,
-	}
-	s.renewExpirationScheduler(l) //自动续期程序
-	return l, nil
+type t1 struct {
+	ch  chan struct{}
+	num atomic.Int64
 }
 
 // Lock 阻塞获取锁，直到获取成功或遇到错误才返回
 // 可以自动续期
 func (s *RedisSync) Lock(key string) (*Lock, error) {
-	ttl := time.Second * time.Duration(rand.Intn(11)+20)                                //存储时长秒 最少25秒 最大30秒
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*86400*365*100) //这里设置超时时间为100年,也就是必须获取到锁才返回，否则一直阻塞
-
-	var err error
-	var rdlLock *redislock.Lock
+	s.lock.Lock()
+	if _, ok := s.m[key]; ok == false {
+		s.m[key] = &t1{ch: make(chan struct{}, 1), num: atomic.Int64{}}
+	}
+	s.m[key].num.Add(1)
+	s.lock.Unlock()
 
 	metadata := getParentCaller()
 
-	rdlLock, err = s.redisLockClient.Obtain(ctx, key, ttl, &redislock.Options{
+	s.println(metadata, "阻塞等待1")
+	s.m[key].ch <- struct{}{}
+	s.println(metadata, "阻塞等待2")
+
+	ttl := time.Second * time.Duration(rand.Intn(11)+20)                                //存储时长秒 最少25秒 最大30秒
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*86400*365*100) //这里设置超时时间为100年,也就是必须获取到锁才返回，否则一直阻塞
+
+	rdlLock, err := s.redisLockClient.Obtain(ctx, key, ttl, &redislock.Options{
 		//重试策略 默认最多只等待ttl秒或设置 context.WithTimeout 来控制尝试时长
-		RetryStrategy: redislock.ExponentialBackoff(time.Millisecond*100, time.Second*10), //指数退避算法(最小间隔时间，最大间隔时间)
+		RetryStrategy: redislock.LinearBackoff(time.Millisecond * 50), //100毫秒重试1次
 		Metadata:      metadata,
 	})
 	if err != nil {
-		cancel()
+		cancel() //获取锁失败 走到这一般是redis重启之类的
 		return nil, err
 	}
 
+	s.println(metadata, "加锁成功", s.m)
+
 	l := &Lock{
-		rdl:      rdlLock,
-		ctx:      ctx,
-		key:      key,
-		cancel:   cancel,
-		ttl:      ttl,
-		metadata: metadata,
+		redisSync: s,
+		rdl:       rdlLock,
+		ctx:       ctx,
+		key:       key,
+		cancel:    cancel,
+		ttl:       ttl,
+		metadata:  metadata,
 	}
 	s.renewExpirationScheduler(l) //自动续期程序
 	return l, nil
@@ -167,39 +125,4 @@ func (s *RedisSync) renewExpirationScheduler(l *Lock) {
 			}
 		}
 	}()
-}
-
-// 日志打印
-func (s *RedisSync) println(v ...any) {
-	if s.logger != nil {
-		s.logger.Println(v...)
-	}
-}
-
-// GetParentCaller 获取父级调用者所在行号
-func getParentCaller(dirLevel ...int) string {
-	s := 3 //默认为父级调用者
-	if len(dirLevel) > 0 {
-		s = dirLevel[0]
-	}
-	_, file, line, ok := runtime.Caller(2)
-	if ok == true {
-		return fmt.Sprintf("%v:%v", pathRetainRight(file, s), line)
-	} else {
-		return ""
-	}
-}
-
-// PathRetainRight 保留n级目录
-func pathRetainRight(pathStr string, n int) string {
-	rt := ""
-	for i := 0; i < n; i++ {
-		b := path.Base(pathStr)
-		if b == "." {
-			break
-		}
-		rt = b + "/" + rt
-		pathStr = path.Dir(pathStr)
-	}
-	return strings.Trim(rt, "/")
 }
