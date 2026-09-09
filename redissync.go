bf642ad357679b1d493f9f144ac752b2e519a8df
liujinsuo <1027976838@qq.com>
2026-09-09 11:50:07 +0800

    加锁成功改成debug类型的日志

--- redissync.go
+++ redissync.go
@@ -142,7 +142,7 @@ func (s *RedisSync) internalLock(ctx context.Context, key string) (*Lock, error)
 		clearNumMapFunc()
 		return nil, err
 	}
-	s.info("加锁成功 key:%s caller:%s", key, metadata)
+	s.debug("加锁成功 key:%s caller:%s", key, metadata)
 
 	l := &Lock{
 		redisSync:    s,
